5e1eb0222e113f5b92a7f0168541139135ca3aec
Jonathan Jay <56273460+Jonathan-Jay@users.noreply.github.com>
2026-08-24 16:39:06 -0400

    sidebar test

--- js/main.js
+++ js/main.js
@@ -3,3 +3,16 @@ const URL = "https://jonathan-jay.github.io/";
 function changeURL(address) {
 	document.location.href = URL + address;
 }
+
+function insertSidebar() {
+	var sidebar = document.createElement("aside");
+	sidebar.id = "sidebar";
+	sidebar.className = "sidebar";
+	sidebar.innerHTML = `<div style="text-align: center; font-size: 2rem;">Sidebar</div>
+		<div>test</div>
+		<div>test</div>
+		<div>test</div>
+		<div>test</div>
+	`;
+	document.body.insertBefore(sidebar, document.getElementById("main"));
+}
